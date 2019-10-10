af0c1dab5abaae69571ab68abc3432ec4c1c676e
angelique-w <angelique.wons@gmail.com>
2019-10-10 15:18:32 +0200

    created contactlist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import Contact from './components/Contact'
+import ContactList from './components/ContactList'
 
 
 function App() {
   return (
     <div className="App">
-      <Contact 
-      name="Melissa West"
-      image="https://randomuser.me/api/portraits/women/55.jpg"
-      online
-      />
-      <Contact 
-      name="Ted Myers"
-      image="https://randomuser.me/api/portraits/men/52.jpg"
-      />
-      <Contact 
-      name="Don Spencer"
-      image="https://randomuser.me/api/portraits/men/82.jpg"
-      online
+      <ContactList />
       />
     </div>
   );
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Contact from './Contact'
+
+const contacts = [
+    {
+        name: "Melissa West",
+        image: "https://randomuser.me/api/portraits/women/55.jpg",
+        online: true
+    },
+    {
+        name: "Ted Myers",
+        image: "https://randomuser.me/api/portraits/men/52.jpg",
+        online: false
+    },
+    {
+        name: "Don Spencer",
+        image: "https://randomuser.me/api/portraits/men/82.jpg",
+        online: true
+    }, 
+    {
+        name: "Becky Jennings",
+        image: "https://randomuser.me/api/portraits/women/82.jpg",
+        online : true
+    },
+    {
+        name: "Jacob Phillips",
+        image: "https://randomuser.me/api/portraits/men/97.jpg", 
+        online: false
+    }
+];
+
+const ContactList = () => (
+    <div>
+        {contacts.map(item => (
+            <Contact name={item.name} image={item.image} online={item.online} />
+        ))}
+    </div>
+);
+
+export default ContactList;
