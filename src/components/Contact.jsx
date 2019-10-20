63bcc64b51c5d25698ed9b07657c431fb63b46d7
angelique-w <angelique.wons@gmail.com>
2019-10-20 16:10:16 +0200

    transform Contact to class Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,45 @@
 import React from 'react';
 import './Contact.css';
 
-const Contact = ({image, name, online}) => {
-    const isOnline = online ? "status-online" : "status-offline";
-    const textIsOnline = online ? "Online" : "Offline";
-    return (
-    <div className="Contact">
-        <img className="avatar" src={image} alt={name}></img>
-        <div>
-            <p className="name">{name}</p>
-            <p className="status">
-                <span className={isOnline}></span>
-                <span className="status-text">
-                    {textIsOnline}
-                </span>
+// const Contact = ({image, name, online}) => {
+//     const isOnline = online ? "status-online" : "status-offline";
+//     const textIsOnline = online ? "Online" : "Offline";
+//     return (
+//     <div className="Contact">
+//         <img className="avatar" src={image} alt={name}></img>
+//         <div>
+//             <p className="name">{name}</p>
+//             <p className="status">
+//                 <span className={isOnline}></span>
+//                 <span className="status-text">
+//                     {textIsOnline}
+//                 </span>
                 
-            </p>
-        </div>
+//             </p>
+//         </div>
 
-    </div>
-    )
+//     </div>
+//     )
+// }
+
+class Contact extends React.Component {
+
+    render() {
+        <div className="Contact">
+            <img className="avatar" src={this.props.image} alt={this.props.name}></img>
+            <div>
+                <p className="name">{this.props.name}</p>
+                <p className="status">
+                    <span className={this.props.isOnline}></span>
+                    <span className="status-text">
+                        {textIsOnline}
+                    </span>
+                </p>
+            </div>
+        </div>
+    }
+        
+    
 }
 
 export default Contact;
